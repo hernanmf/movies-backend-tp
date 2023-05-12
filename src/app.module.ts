a9c96672e905c94100d045cee9b21fab3dae714d
Hernan Martinez <hernanmartinezf@gmail.com>
2023-05-12 20:07:53 -0300

    generado recurso peliculas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PeliculasModule } from './peliculas/peliculas.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PeliculasModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
